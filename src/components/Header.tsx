19633fed3aad44f53cd7afa9f45630892fadb5f5
henriquepablo <pablo269704@gmail.com>
2024-08-06 15:11:41 -0300

    feat: adding header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/HeaderStyle.css';
+
+function Header() {
+    return(
+        <div className="header">
+            <h1 className="title">
+                BlogL
+            </h1>
+        </div>
+    );
+}
+
+export default Header;
